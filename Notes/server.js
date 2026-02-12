3a2ef9771912bbf9959530857810aa0572a94250
Azhaan Ali Ansari <azhaanaliansari@gmail.com>
2026-02-12 15:34:23 +0530

    Update PORT and start scripts for deployment

--- Notes/server.js
+++ Notes/server.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./config/databse');
+const PORT = process.env.PORT || 3000;
 
 
 connectDB();
 
 
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
